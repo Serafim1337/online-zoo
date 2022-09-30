88926d1e05f9bed3eddc60a1e65331950d7205bb
Serafim <7620226@gmail.com>
2022-10-01 01:13:54 +0300

    feat: add 1000px,640px for main page

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,1 +1,7 @@
-let a = 0;
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenu.addEventListener("click", burgerMenuHandler);
+
+function burgerMenuHandler() {
+  burgerMenu.classList.toggle("cross");
+}
