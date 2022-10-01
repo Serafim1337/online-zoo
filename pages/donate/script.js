30a692d550fb9d191dc09177b6404f34c894d8a5
Serafim <7620226@gmail.com>
2022-10-01 04:24:12 +0300

    feat: add 1000px,640px,320px donation page; fixed 320px main page

---
+++ pages/donate/script.js
@@ -0,0 +1,7 @@
+const burgerMenu = document.querySelector(".burger-menu");
+
+burgerMenu.addEventListener("click", burgerMenuHandler);
+
+function burgerMenuHandler() {
+  burgerMenu.classList.toggle("cross");
+}
