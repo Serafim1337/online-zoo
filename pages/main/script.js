fde3e4432474103017fd231bcfa31d20a98292e5
Serafim <7620226@gmail.com>
2022-10-08 18:42:53 +0300

    feat: add dropdown menu on burger menu button both pages

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,7 +1,30 @@
 const burgerMenu = document.querySelector(".burger-menu");
+const overlay = document.querySelector(".overlay");
+const body = document.body;
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const navLinks = dropdownMenu.querySelectorAll(".nav-link");
+const dropdownLogo = dropdownMenu.querySelector(".dropdown-logo");
 
 burgerMenu.addEventListener("click", burgerMenuHandler);
 
 function burgerMenuHandler() {
   burgerMenu.classList.toggle("cross");
+  body.classList.toggle("lock");
+  overlay.hidden = !overlay.hidden;
+  dropdownMenu.hidden = !dropdownMenu.hidden;
+}
+
+overlay.addEventListener("click", function (e) {
+  switch (e.currentTarget) {
+    case overlay:
+    case burgerMenu:
+      burgerMenuHandler();
+      break;
+  }
+});
+
+dropdownLogo.onclick = burgerMenuHandler;
+
+for (let link of navLinks) {
+  link.onclick = burgerMenuHandler;
 }
