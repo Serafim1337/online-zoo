6d716ae8ff63fa59cdfab25e0b7a31389beca094
Serafim <7620226@gmail.com>
2022-10-09 16:16:25 +0300

    feat: implemented popup on testimonials

--- pages/main/popupWindow.js
+++ pages/main/popupWindow.js
@@ -4,17 +4,39 @@ const cardsSection = document.querySelector(".testimonials-cards");
 cardsSection.addEventListener("click", function (e) {
   if (!e.target.closest(".testimonial-card")) return;
   if (document.documentElement.clientWidth > 980) return;
-  showOverlay();
-  showPopup(e.target.closest(".testimonial-card"));
+  toggleOverlay();
+  togglePopup(e.target.closest(".testimonial-card"));
 });
 
-function showOverlay() {
+overlay.addEventListener("click", function (e) {
+  if (!e.target.closest(".popup-card")) {
+    toggleOverlay();
+    togglePopup(document.querySelector(".popup-card"));
+  }
+});
+
+function toggleOverlay() {
   overlay.hidden = !overlay.hidden;
   body.classList.toggle("lock");
 }
 
-function showPopup(card) {
+function togglePopup(card) {
   card.classList.toggle("popup-card");
-  overlay.before(card);
-  card.firstElementChild.classList.toggle("popup-card-colored");
+  if (card.classList.contains("popup-card")) {
+    overlay.before(card);
+    card.firstElementChild.classList.toggle("popup-card-colored");
+    card.addEventListener("click", function (e) {
+      closePopupButtonHandler(e, card);
+    });
+  } else {
+    let cardParent = card.parentElement;
+    cardParent.removeChild(card);
+  }
+}
+
+function closePopupButtonHandler(e, card) {
+  if (e.target.classList.contains("popup-card")) {
+    toggleOverlay();
+    togglePopup(card);
+  }
 }
